067d39c31b5382386d8df7990e2a595ecc11b2e9
Andrew Chon <andrewdchon@gmail.com>
2020-07-02 15:09:33 -0500

    updated lang

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const getSubtitles = require('youtube-captions-scraper').getSubtitles;
 
+router.get('/', (req, res) => {
+	res.json('hi')
+})
+
 router.get('/api/:id', async (req, res) => {
 	getSubtitles({
 		videoID: req.params.id, // youtube video id
-		lang: 'en', // default: `en`
+		lang: 'ko', // default: `en`
 	}).then(function (captions) {
 		res.json(captions);
 	});
